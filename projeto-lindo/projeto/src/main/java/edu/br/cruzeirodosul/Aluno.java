79a5ae76193242703ddd1472faeff342e2efb373
Renan01032 <re.silva@icloud.com>
2025-04-24 19:47:36 -0300

    comit estrutura

---
+++ projeto-lindo/projeto/src/main/java/edu/br/cruzeirodosul/Aluno.java
@@ -0,0 +1,10 @@
+package edu.br.cruzeirodosul;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno {
+    private String nome;
+}
---
+++ projeto-lindo/projeto/src/main/java/edu/br/cruzeirodosul/Main.java
@@ -0,0 +1,9 @@
+package edu.br.cruzeirodosul;
+
+public class Main {
+    public static void main(String[] args) {
+        Aluno a = new Aluno();
+        a.setNome("Naruto");
+        System.out.println(a.getNome());
+    }
+}
